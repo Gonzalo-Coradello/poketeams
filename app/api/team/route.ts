fed8d5719148c392aa47dd25217be80b2d415a6d
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-03-19 17:15:07 -0300

    add prisma and fetch teams from db

---
+++ app/api/team/route.ts
@@ -0,0 +1,13 @@
+import prisma from "@/lib/prisma";
+
+export default function POST(req: Request, res: Response) {
+
+    // const team = req.body.team
+
+    // const result = await prisma.team.create({
+    //     data: {
+
+    //     }
+    // })
+
+}
---
+++ app/teams/page.tsx
@@ -0,0 +1,22 @@
+import Team from "@/components/Team";
+import prisma from "@/lib/prisma";
+
+export default async function Teams() {
+  const teams = await prisma.team.findMany({
+    include: {
+      team: true,
+    },
+  });
+
+  return (
+    <div>
+      <h1>Teams</h1>
+
+      <div>
+        {teams.map((team) => (
+          <Team key={team.id} { ...team } />
+        ))}
+      </div>
+    </div>
+  );
+}
