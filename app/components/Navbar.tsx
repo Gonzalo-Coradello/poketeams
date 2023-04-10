8490ff9ca4a878a60ffb3ee24fec7512bf390bde
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-04-10 16:35:03 -0300

    modified navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,21 +5,21 @@ import Image from "next/image"
 export default function Navbar() {
 
     const links = [
-        {url: '/pokedex', slug: 'Pokedex'},
+        {url: '/pokedex', slug: 'Pokédex'},
         {url: '/teams', slug: 'Teams'},
         {url: '/teams/create', slug: 'Create team'},
     ]
 
     return (
     <header>
-        <nav className="py-8 border-b-2">
+        <nav className="font-semibold py-8 border-b-2 sm:text-lg">
             <div className="flex justify-center gap-8 sm:justify-between">
-                <div className="flex">
+                <Link href="/" className="flex items-center gap-[2px] hover:opacity-50 transition-opacity duration-300">
                     <h1 className="hidden sm:block">Pokéteams</h1>
                     <Image src={pokeball} alt="pokeball" width={25} height={25} />
-                </div>
+                </Link>
                 <ul className="flex gap-8">
-                    { links.map(link => <li key={link.slug}><Link href={link.url}>{ link.slug }</Link></li>) }
+                    { links.map(link => <li key={link.slug}><Link href={link.url} className="hover:opacity-50 transition-opacity duration-300" >{ link.slug }</Link></li>) }
                 </ul>
             </div>
         </nav>
