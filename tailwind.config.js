a7a5c20bd4198a1ecca94a24c0f05bdcb4c86347
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-04-10 18:26:25 -0300

    add classes to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,5 +33,5 @@ module.exports = {
     },
   },
   plugins: [],
-  safelist: [ "bg-normal", "bg-fire", "bg-water", "bg-grass", "bg-electric", "bg-ice", "bg-fighting", "bg-poison", "bg-ground", "bg-flying", "bg-psychic", "bg-bug", "bg-rock", "bg-ghost", "bg-dark", "bg-dragon", "bg-steel", "bg-fairy", "text-fire", "border-fire", "text-grass", "border-grass", "text-water", "border-water" ]
+  safelist: [ "bg-normal", "bg-fire", "bg-water", "bg-grass", "bg-electric", "bg-ice", "bg-fighting", "bg-poison", "bg-ground", "bg-flying", "bg-psychic", "bg-bug", "bg-rock", "bg-ghost", "bg-dark", "bg-dragon", "bg-steel", "bg-fairy", "text-fire", "border-fire", "text-grass", "border-grass", "text-water", "border-water", "hover:bg-fire", "hover:text-fire", "hover:bg-grass", "hover:text-grass", "hover:bg-water", "hover:text-water", "hover:text-white", "hover:text-black" ]
 }
