9a60f7401aa341078e69969efa0cddcd1ce344aa
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-04-10 16:35:22 -0300

    customize link button component

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
 
-export default function LinkButton({ href, children }: { href: string, children: React.ReactNode }) {
-  return <Link className="w-fit mx-auto px-4 py-2 border rounded-md hover:bg-white hover:text-black transition-all duration-300" href={href}>{children}</Link>
+type Props = {
+  href: string,
+  color?: string,
+  children: React.ReactNode
+}
+
+export default function LinkButton({ href, color = "white", children }: Props) {
+  return <Link className={`text-${color} w-fit mx-auto px-4 py-2 border border-${color} rounded-md hover:bg-${color} hover:text-${color === "white" ? "black" : "white"} transition-all duration-300`} href={href}>{children}</Link>
 }
